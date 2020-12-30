79a9d67a0f55f9f31c5eb934539b2f87723584bc
lukescook <34013390+lukescook@users.noreply.github.com>
2020-12-30 02:22:19 +0000

    Update MeetingDB.java

--- java/MeetingDB.java
+++ java/MeetingDB.java
@@ -1,10 +1,10 @@
-package com.example.a906771.projectmeetingapp;
+package com.example.a.projectmeetingapp;
 
 /**
  * MeetingDB.java
  * contains meeting database table information
  *
- * @author Luke Cook (906771)
+ * @author Luke Cook
  */
 public class MeetingDB {
     // Labels table name
